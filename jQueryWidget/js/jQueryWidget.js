334d03984aba96e7d6bf8d29991354eba1554ff8
jhilde7190 <57957233+jhilde7190@users.noreply.github.com>
2019-11-20 15:46:55 -0600

    Update jQueryWidget/js/jQueryWidget.js
    Moved JavaScript from jquerywidget.html to jQueryWidget/js/jQueryWidget.js

--- jQueryWidget/js/jQueryWidget.js
+++ jQueryWidget/js/jQueryWidget.js
@@ -1,1 +1,55 @@
+$(function() {
+  $.widget("custom.newsfeed", {
+    options: {
+      domain: "",
+      title: "",
+      url: ""
+    },
 
+    _create: function(){
+      this._refresh();
+    },
+
+    _refresh: function() {
+      $(this.element).html(
+        "<a href=" + this.options.url + " target=\"_blank\">" +
+          "<div class=\"title\"><h2>" + this.options.title + "</h2></div>" +
+          "<div class=\"domain\">" + this.options.domain + "</div>" +
+        "</a>"
+      );
+
+      $(this.element.children()[0]).button();
+      $(this.element).show();
+    },
+
+    story: function(news_story) {
+      this.options = news_story;
+      this._refresh();
+    },
+
+    _destroy: function() {
+      $(this.element).html();
+    }
+  });
+
+  $.getJSON("https://www.reddit.com/r/worldnews.json", function(data) {
+    var story = {};
+    $.each(data.data.children, function(index, value) {
+      if (index < 5) {
+        story = {
+          domain: this.data.domain,
+          title: this.data.title,
+          url: this.data.url
+        };
+
+        $("#news_story" + index).newsfeed();
+
+        $("#news_story" + index).newsfeed("story", story);
+      } else {
+        return false;
+      }
+    });
+  });
+
+  $(".widget .news_story a").button();
+});
